df6455d6b46102f836fe4c2f0e20da4cc3c87ffe
Abhishek Gupta <guptab02@outlook.com>
2024-12-21 16:59:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,8 @@ if st.button("Predict AdViews"):
 	}
 	
     input_data = pd.DataFrame([input_data])
-    
+    print(input_data)
+	
     #category={'Fitness': 1,'Music':2,'Travel':3,'Electronics':4,'Movie':5,'Fashion':6,'Vlog':7,'Comedy':8}
     #input_data["category"] = input_data["category"].map(category)
 	
