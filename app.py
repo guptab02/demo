486bd7fc2abc2aca510b31ac886fdb8302ce1723
Abhishek Gupta <guptab02@outlook.com>
2024-12-21 15:13:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pickle
 import pandas as pd
 import numpy as np
 import datetime
+import sklearn
 import time
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
